a2397dc2414a85c1f922a8e1e51eed79b87b7082
kalljohn <kalljohn@gmail.com>
2015-07-29 01:01:30 +0800

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+sudo apt-get install wget curl
+sudo apt-get install tmux
+
+sudo apt-get install git
+
+sudo apt-get install openssh-server openssh-client
+
+sudo apt-get install vim vim-gnome ctags cscope
+sudo apt-get install vim-youcomplete vim-addon-manager
+
+sudo apt-get install python python-pip
+
+sudo apt-get install cmake gcc g++ automake autoconf
+
+wget -qO- https://get.docker.com/ | sh
