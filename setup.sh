c715b165d8f7c47fef612cdfc1b8df660c8540b6
kalljohn <kalljohn@gmail.com>
2015-08-14 00:14:11 +0800

    add dos2unix, pip install thefuck.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 sudo apt-get install wget curl most
 sudo apt-get install tmux
+sudo apt-get install dos2unix
 
 sudo apt-get install git
 
@@ -11,6 +12,7 @@ sudo apt-get install vim vim-gnome ctags cscope
 sudo apt-get install vim-youcomplete vim-addon-manager
 
 sudo apt-get install python python-pip
+sudo pip install thefuck
 
 sudo apt-get install cmake gcc g++ automake autoconf
 
