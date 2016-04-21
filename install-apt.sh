a664c19d544a16a4bd85c16739afc8afd9e48e1c
kalljohn <kalljohn@gmail.com>
2016-04-21 14:19:29 +0800

    update script

--- install-apt.sh
+++ install-apt.sh
@@ -9,11 +9,18 @@ sudo apt-get install git
 sudo apt-get install openssh-server openssh-client
 
 sudo apt-get install vim vim-gnome ctags cscope
-sudo apt-get install vim-youcomplete vim-addon-manager
+sudo apt-get install vim-addon-manager
 
-sudo apt-get install python python-pip
+sudo apt-get install python python-pip python-dev
 sudo pip install thefuck
 
-sudo apt-get install cmake gcc g++ automake autoconf
+sudo apt-get install \
+    build-essential \
+    cmake \
+    gcc \
+    g++ \
+    automake \
+    autoconf \
+    swig
 
 wget -qO- https://get.docker.com/ | sh
