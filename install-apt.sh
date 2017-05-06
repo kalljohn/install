09eb09694eb0c1bbb764fa28ff4506d9ef3d45b2
Benson Lin <bensonlin@qnap.com>
2017-05-06 23:58:35 +0800

    update packages

--- install-apt.sh
+++ install-apt.sh
@@ -1,20 +1,31 @@
 #!/bin/sh
 
-apt-get -y install wget curl most
-apt-get -y install tmux
-apt-get -y install dos2unix
-apt-get -y install guake
+sudo apt-add-repository ppa:fish-shell/release-2
+sudo apt update
+sudo apt -y install fish
+sudo chsh -s /usr/bin/fish
 
-apt-get -y install git
+sudo apt -y install wget curl most
+sudo apt -y install tmux
+sudo apt -y install dos2unix
+sudo apt -y install guake
+sudo apt -y install cde
 
-apt-get -y install openssh-server openssh-client
+sudo apt -y install git
 
-apt-get -y install vim vim-gnome ctags cscope
-apt-get -y install vim-addon-manager
+sudo apt -y install openssh-server openssh-client
 
-apt-get -y install python2.7 python2.7-dev python-pip
-pip install thefuck
+sudo apt -y install vim vim-gnome ctags cscope
+sudo apt -y install vim-addon-manager
 
-apt-get -y install build-essential cmake gcc g++ automake autoconf swig clang
+sudo apt -y install python python-pip python-dev
+sudo pip install thefuck
+
+sudo apt -y install \
+    build-essential cmake \
+    gcc g++ clang \
+    automake autoconf
+
+sudo apt -y install swig cde
 
 wget -qO- https://get.docker.com/ | sh
