1bc8c94b4b16e99ecae7e2f2b1651fe553c92562
kalljohn <kalljohn@gmail.com>
2018-04-14 19:41:37 +0800

    Delete clone-github.sh

--- clone-github.sh
+++
@@ -1,6 +0,0 @@
-#!bin/sh
-
-git clone https://github.com/kalljohn/bash-conf ~/.bash-conf
-git clone https://github.com/kalljohn/tmux-conf ~/.tmux-conf
-git clone https://github.com/kalljohn/vimfiles ~/.vim
-
